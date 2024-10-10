3981047f4944ceab51bfff25434d7e409ecbd940
YuXiao <wangyuxiao@bitdesk.com>
2024-10-10 23:24:01 +0800

    Add BitDesk Code

--- modules/single_pool.py
+++ modules/single_pool.py
@@ -11,7 +11,7 @@ class SingleNominatorModule(PoolModule):
     description = 'Orbs\'s single nominator pools.'
     default_value = False
 
-    def do_create_single_pool(self, pool_name, owner_address):
+    def do_create_single_pool(self, pool_name, owner_wallet_name):
         self.ton.local.add_log("start create_single_pool function", "debug")
 
         self.check_download_pool_contract_scripts()
@@ -23,10 +23,8 @@ class SingleNominatorModule(PoolModule):
 
         fift_script = pkg_resources.resource_filename('mytoncore', 'contracts/single-nominator-pool/init.fif')
         code_boc = pkg_resources.resource_filename('mytoncore', 'contracts/single-nominator-pool/single-nominator-code.hex')
-        validator_wallet = self.ton.GetLocalWallet("GtNode-0")
-        print(dir(validator_wallet), validator_wallet.addrB64)
-        validator_wallet = self.ton.GetValidatorWallet()
-        args = [fift_script, code_boc, owner_address, validator_wallet.addrB64, file_path]
+        validator_wallet = self.ton.GetLocalWallet(owner_wallet_name)
+        args = [fift_script, code_boc, validator_wallet.addrB64, validator_wallet.addrB64, file_path]
         print(" ".join(args))
         result = self.ton.fift.Run(args)
         if "Saved single nominator pool" not in result:
@@ -42,11 +40,11 @@ class SingleNominatorModule(PoolModule):
     def new_single_pool(self, args):
         try:
             pool_name = args[0]
-            owner_address = args[1]
+            owner_wallet_name = args[1]
         except:
-            color_print("{red}Bad args. Usage:{endc} new_single_pool <pool-name> <owner_address>")
+            color_print("{red}Bad args. Usage:{endc} new_single_pool <pool-name> <owner_wallet_name>")
             return
-        self.do_create_single_pool(pool_name, owner_address)
+        self.do_create_single_pool(pool_name, owner_wallet_name)
         color_print("new_single_pool - {green}OK{endc}")
 
     def do_activate_single_pool(self, pool):
